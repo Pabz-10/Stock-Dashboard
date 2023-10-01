0390b9bee303e2a587a68cfde0ef4d5b904aab3f
Pabil <pabil.adhikari10@gmail.com>
2023-09-30 20:59:48 -0700

    Added multiple components and eddited dark mode button styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Search from './Search'
+import React from "react";
+import Search from "./Search";
+import ThemeIcon from "./ThemeIcon";
 
-const Header = ( {name}) => {
-  return (
-    <>
-        <div className='xl:px-32'>
-            <h1 className="text-5xl">{name}</h1>
-            <Search />
-        </div>
-    </>
-  )
-}
+const Header = ({ name }) => {
+	return (
+		<>
+			<div className="xl:px-32">
+				<h1 className="text-5xl">{name}</h1>
+				<Search />
+			</div>
+			<ThemeIcon />
+		</>
+	);
+};
 
-export default Header
+export default Header;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,53 +1,59 @@
-import React, { useState } from "react"
-import { mockSearchResults } from "../constants/mock"
-import {XIcon, SearchIcon} from "@heroicons/react/solid"
-
+import React, { useState } from "react";
+import { mockSearchResults } from "../constants/mock";
+import { XIcon, SearchIcon } from "@heroicons/react/solid";
+import { SearchResults } from "./SearchResults";
 
 const Search = () => {
-    const [input, setInput] = useState("");
-    // tracks users query for which stock theyre looking for
-    const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
-   
-    const clear = () => {
-        //clears our states 
-        setInput("");
-        setBestMatches([]);
-    };
+	const [input, setInput] = useState("");
+	// tracks users query for which stock theyre looking for
+	const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
 
-    const updateBestMatches = () => {
-        setBestMatches(mockSearchResults.result);
-    };
+	const clear = () => {
+		//clears our states
+		setInput("");
+		setBestMatches([]);
+	};
 
-  return (
-    <div className=" flex items-center my-4 border-2 rounded-md relative z-50 w-95 bg-white border-neutral-200">
-        <input 
-            type="text" 
-            value={input} className="w-full px-4 py-2 focus:outline-none rounded-md"
-            placeholder="Search stock....."
-            onChange={(event) => {
-                setInput(event.target.value);
-            }}
-            // onChenage sets our input state value to whatever is typed onto our input element
-            onKeyPress={(event) => {
-                if (event.key === "Enter") {
-                    updateBestMatches();
-                }
-            }}
-        />
+	const updateBestMatches = () => {
+		setBestMatches(mockSearchResults.result);
+	};
 
+	return (
+		<div className=" flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200">
+			<input
+				type="text"
+				value={input}
+				className="w-full px-4 py-2 focus:outline-none rounded-md"
+				placeholder="Search stock....."
+				onChange={(event) => {
+					setInput(event.target.value);
+				}}
+				// onChenage sets our input state value to whatever is typed onto our input element
+				onKeyPress={(event) => {
+					if (event.key === "Enter") {
+						updateBestMatches();
+					}
+				}}
+			/>
 
-        {input && (
-            <button onClick={clear}>
-                <XIcon className="h-4 w-4 fill-gray-500" />
-            </button>
-        )}
+			{input && (
+				<button onClick={clear} className="m-1">
+					<XIcon className="h-4 w-4 fill-gray-500" />
+				</button>
+			)}
 
+			<button
+				onClickCapture={updateBestMatches}
+				className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2"
+			>
+				<SearchIcon className="h-4 w-4 fill-gray-100" />
+			</button>
 
-        <button onClickCapture={updateBestMatches} className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2">
-            <SearchIcon className="h-4 w-4 fill-gray-100"/>
-        </button>
-    </div>
-    )
+			{input && bestMatches.length > 0 ? (
+				<SearchResults results={bestMatches} />
+			) : null}
+		</div>
+	);
 };
 
-export default Search
+export default Search;
