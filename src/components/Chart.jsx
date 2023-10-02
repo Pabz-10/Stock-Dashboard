29398f7ae473301903cff69f0d53651a9418bbbe
Pabil <pabil.adhikari10@gmail.com>
2023-10-01 21:14:20 -0700

    Added button theme

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -11,6 +11,8 @@ import {
 	YAxis,
 } from "recharts";
 import { data } from "autoprefixer";
+import ChartFilter from "./ChartFilter";
+import { chartConfig } from "../constants/config";
 
 const Chart = () => {
 	const [date, setDate] = useState(mockHistroicalData);
@@ -22,20 +24,43 @@ const Chart = () => {
 				value: item.toFixed(2),
 				// item with 2 decimal places
 				date: convertUnixTimestampToDate(date.t[index]),
-				
 			};
 		});
 	};
 	return (
 		<Card>
+			<ul className="flex absolute top-2 right-2 z-40">
+				{Object.keys(chartConfig).map((item) => {
+					return (
+						<li key={item}>
+							<ChartFilter
+								text={item}
+								active={filter === item}
+								onClick={() => setFilter(item)}
+							/>
+						</li>
+					);
+				})}
+			</ul>
 			<ResponsiveContainer>
 				<AreaChart data={formatData(data)}>
+					<defs>
+						<linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+							<stop
+								offset="5%"
+								stopColor="rgb(199 210 254)"
+								stopOpacity={0.8}
+							/>
+							<stop offset="95%" stopColor="rgb(199 210 254)" stopOpacity={0} />
+						</linearGradient>
+					</defs>
 					<Area
 						type="monotone"
 						dataKey="value"
 						stroke="#312e81"
 						fillOpacity={1}
 						strokeWidth={0.5}
+						fill="url(#chartColor)"
 					/>
 					<Tooltip />
 					<XAxis dataKey={"date"} />
--- src/components/ThemeIcon.jsx
+++ src/components/ThemeIcon.jsx
@@ -1,10 +1,27 @@
 import { MoonIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 const ThemeIcon = () => {
+	const { darkMode, setDarkMode } = useContext(ThemeContext);
+
+	const toggleDarkMode = () => {
+		setDarkMode(!darkMode);
+	};
 	return (
-		<button className="rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 top-10 xl:top-10 shadow-lg">
-			<MoonIcon className="h-8 w-8 cursor-pointer stroke-1 fill-none stroke-neutral-400 " />
+		<button
+			className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 top-10 xl:top-10 shadow-lg ${
+				darkMode ? "shadow-gray-100" : null
+			}`}
+			onClick={toggleDarkMode}
+		>
+			<MoonIcon
+				className={`h-8 w-8 cursor-pointer stroke-1 fill-none ${
+					darkMode
+						? "fill-yellow-400 stroke-yellow-400"
+						: "fill-none  stroke-neutral-400"
+				}`}
+			/>
 		</button>
 	);
 };
