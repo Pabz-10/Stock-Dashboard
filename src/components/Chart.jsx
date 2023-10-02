c3bfe8fb8a6c3433b89fa59d7a854c9688d87308
Pabil <pabil.adhikari10@gmail.com>
2023-10-02 01:52:59 -0700

    Updated chat functions

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,6 +1,9 @@
-import React, { useContext, useState } from "react";
-import { mockHistroicalData } from "../constants/mock";
-import { convertUnixTimestampToDate } from "../helpers/date-helper";
+import React, { useContext, useState, useEffect } from "react";
+import {
+	convertUnixTimestampToDate,
+	convertDateToUnixTimestamp,
+	createDate,
+} from "../helpers/date-helper";
 import Card from "./Card";
 import {
 	Area,
@@ -10,43 +13,79 @@ import {
 	XAxis,
 	YAxis,
 } from "recharts";
-import { data } from "autoprefixer";
 import ChartFilter from "./ChartFilter";
 import { chartConfig } from "../constants/config";
 import ThemeContext from "../context/ThemeContext";
+import { fetchHistoricalData } from "../api/Stock-api";
+import StockContext from "../context/StockContext";
 
 const Chart = () => {
-	const [date, setDate] = useState(mockHistroicalData);
+	const [data, setData] = useState([]);
+
 	const [filter, setFilter] = useState("1W");
 
 	const { darkMode } = useContext(ThemeContext);
 
-	const formatData = () => {
-		return date.c.map((item, index) => {
+	const { stockSymbol } = useContext(StockContext);
+
+	const formatData = (data) => {
+		return data.c.map((item, index) => {
 			return {
 				value: item.toFixed(2),
-				// item with 2 decimal places
-				date: convertUnixTimestampToDate(date.t[index]),
+				date: convertUnixTimestampToDate(data.t[index]),
 			};
 		});
 	};
+
+	useEffect(() => {
+		const getDateRange = () => {
+			const { days, weeks, months, years } = chartConfig[filter];
+
+			const endDate = new Date();
+			const startDate = createDate(endDate, -days, -weeks, -months, -years);
+
+			const startTimestampUnix = convertDateToUnixTimestamp(startDate);
+			const endTimestampUnix = convertDateToUnixTimestamp(endDate);
+			return { startTimestampUnix, endTimestampUnix };
+		};
+
+		const updateChartData = async () => {
+			try {
+				const { startTimestampUnix, endTimestampUnix } = getDateRange();
+				const resolution = chartConfig[filter].resolution;
+				const result = await fetchHistoricalData(
+					stockSymbol,
+					resolution,
+					startTimestampUnix,
+					endTimestampUnix
+				);
+				setData(formatData(result));
+			} catch (error) {
+				setData([]);
+				console.log(error);
+			}
+		};
+
+		updateChartData();
+	}, [stockSymbol, filter]);
+
 	return (
 		<Card>
 			<ul className="flex absolute top-2 right-2 z-40">
-				{Object.keys(chartConfig).map((item) => {
-					return (
-						<li key={item}>
-							<ChartFilter
-								text={item}
-								active={filter === item}
-								onClick={() => setFilter(item)}
-							/>
-						</li>
-					);
-				})}
+				{Object.keys(chartConfig).map((item) => (
+					<li key={item}>
+						<ChartFilter
+							text={item}
+							active={filter === item}
+							onClick={() => {
+								setFilter(item);
+							}}
+						/>
+					</li>
+				))}
 			</ul>
 			<ResponsiveContainer>
-				<AreaChart data={formatData(data)}>
+				<AreaChart data={data}>
 					<defs>
 						<linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
 							<stop
@@ -66,14 +105,14 @@ const Chart = () => {
 						dataKey="value"
 						stroke="#312e81"
 						fillOpacity={1}
-						strokeWidth={0.5}
 						fill="url(#chartColor)"
+						strokeWidth={0.5}
 					/>
 					<Tooltip
 						contentStyle={darkMode ? { backgroundColor: "#111827" } : null}
 						itemStyle={darkMode ? { color: "#818cf8" } : null}
 					/>
-					<XAxis dataKey={"date"} />
+					<XAxis dataKey="date" />
 					<YAxis domain={["dataMin", "dataMax"]} />
 				</AreaChart>
 			</ResponsiveContainer>
